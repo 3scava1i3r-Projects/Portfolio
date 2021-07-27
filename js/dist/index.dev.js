64a77f986a626c148e5b85bec8b71ecb2b1e4d49
Hritwik Tripathi <Tripathi.hritwik@gmail.com>
2021-07-27 21:14:52 +0530

    projects section card added

---
+++ js/dist/index.dev.js
@@ -0,0 +1,15 @@
+"use strict";
+
+var section = document.querySelector("section");
+var currentPos = window.pageYOffset;
+
+var update = function update() {
+  var newPos = window.pageYOffset;
+  var diff = newPos - currentPos;
+  var speed = diff * 0.35;
+  section.style.transform = "skewY(".concat(speed, "deg)");
+  currentPos = newPos;
+  requestAnimationFrame(update);
+};
+
+update();
